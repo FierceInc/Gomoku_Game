ddeae1651178b10a93c39b3abf8852caf4874204
FierceInc <fierceincii@gmail.com>
2022-05-09 12:49:16 +0200

    Changed Array Declaration

--- game.h
+++ game.h
@@ -12,12 +12,26 @@ class Game {
     public:
         int BOARD_SIZE;
         int TURN = 0;
+        int **board = new int*[BOARD_SIZE];
+        int ** createBoard(int rows) 
+        {
+            for(int i = 0; i < rows; ++i) {
+            board[i] = new int[rows]; 
+        }
+         for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < rows; j++) {
+            board[i][j] = 0;
+        }
+    }    
+        return board;
+
+}       
     private:
     // Functions
     public:
     // [TODO] Remove the code below this line
     // This print the board ONLY FOR TESTING
-       void printBoard(int board[][100], int board_size){
+       void printBoard(int **board, int board_size){
             for(int rowIndex = 0; rowIndex < board_size; rowIndex++) {
             for(int columnIndex = 0; columnIndex < board_size; columnIndex++) {
             cout<<board[rowIndex][columnIndex]<< " ";
@@ -29,15 +43,18 @@ class Game {
     // [TODO] Remove the code above this line
 
     // Set the move in the board
-    void setMove(int _board[][100],int row, int col, int playerValue) {
-         _board[col][row] = playerValue;
+    void setMove(int **_board,int row, int col, int playerValue) {
+           if(col < BOARD_SIZE && row < BOARD_SIZE) {
+             _board[col][row] = playerValue;
+        }
     }
     // Get value of the board;
-    int getVal(int _board[][100], int row, int col) const {
-		return _board[col][row];
+    int getVal(int **_board, int row, int col){
+        if(col < BOARD_SIZE && row < BOARD_SIZE) {
+            return _board[col][row];
+        }
+        return 0;
 	}
-    // start game;
-    void startGame();
   
 };
 
--- main.cpp
+++ main.cpp
@@ -33,212 +33,143 @@ int * readFromFile() {
 };
 
 // Here you check for wins; By taking the current board and looping through it to find 5 in a row;
-bool checkWin(int board[][100], int playerPiece, int boardSize) 
+bool checkWin(int **board, int playerPiece, int boardSize) 
 {
-     int spaces = boardSize % 5; /* This gets the empty spaces eg in a 6X6 board we only need 1 empty space out of 6 to get 5 in a row so here if board size = 6 you get one if its 7 you get 2 if its 10 you get 0; You just need the remainder in each case*/
+     int spaces = boardSize % 5;
+     for(int col = 0; col < boardSize; col++){
+         for (int row = 0; row < boardSize - spaces; row++){
+             if(board[col][row] == playerPiece && board[col][row + 1] == playerPiece && board[col][row + 2] == playerPiece && board[col][row + 3] == playerPiece && board[col][row + 4] == playerPiece) {
+                 return true;
+             }
+         }    
+     }
+     for(int col = 0; col < boardSize - spaces; col++){
+         for (int row = 0; row < boardSize ; row++){
+             if(col <= spaces ) {
+                    if(board[col][row] == playerPiece && board[col + 1][row] == playerPiece && board[col + 2][row] == playerPiece && board[col + 3][row] == playerPiece && board[col + 4][row] == playerPiece) {
+                                    return true;
+                                }
+             }
+         }    
+     }
 
-    for(int c =  0; c < boardSize - spaces; c++) // Here you subtract the extra spaces you dont need
-    {   
-        for (int r = 0; r < boardSize; r++)
-        {
-
-            // HORIZONTAL CHECK
-           if(board[r][c] == playerPiece && board[r][c + 1] == playerPiece && board[r][c + 2] == playerPiece && board[r][c + 3] == playerPiece && board[r][c + 4] == playerPiece)
-           {
-               return true;
-           }
-            // This is self explaining. You in are going down with rows
-            //VERTICAL CHECK
-           if(board[r][c] == playerPiece && board[r + 1][c] == playerPiece && board[r + 2][c] == playerPiece && board[r + 3][c] == playerPiece && board[r+ 4][c] == playerPiece)
-           {
-               return true;
-           }
-        }
-    }
-// NOW YOU CHECK THE TWO DIAGONALS
-// For POSITIVE DIAGONAL when rows increase columns increase this is what I've done here
-    for(int c =  0; c < boardSize - spaces ; c++)
-     {
-         for (int r = 0; r < boardSize - spaces; r++) // This line is the difference from HORIZONTAL AND VERTICAL
-        {
-            // When row increases column increase;
-            if(board[r][c] == playerPiece && board[r + 1][c + 1] == playerPiece && board[r + 2][c + 2] == playerPiece && board[r + 3][c + 3] == playerPiece && board[r + 4][c + 4] == playerPiece)
-            {
-                return true;
-            }
-        }
-    }
-    //NEGATIVE DIAGONAL
-    for(int c =  0; c < boardSize - spaces ; c++) 
-    {
-        for (int r = spaces; r < boardSize; r++) // When row is equal to the extra spaces. if Column is 0 row is 1
-        {
-
-            //VISE VERSA
-            if(board[r][c] == playerPiece && board[r - 1][c + 1] == playerPiece && board[r - 2][c + 2] == playerPiece && board[r - 3][c + 3] == playerPiece && board[r - 4][c + 4] == playerPiece)
-            {
-                return true;
-            }
-        }
-    }
-    return false; // This whole function returns false if none of the 4 conditions are met;
+      for(int col = 0; col < boardSize - spaces; col++){
+         for (int row = 0; row < boardSize - spaces; row++){
+             if(col <= spaces ) {
+                    if(board[col][row] == playerPiece && board[col + 1][row + 1] == playerPiece && board[col + 2][row + 2] == playerPiece && board[col + 3][row + 3] == playerPiece && board[col + 4][row + 4] == playerPiece) {
+                                    return true;
+                                }
+             }
+         }    
+     }
+ for(int col = 0; col < boardSize; col++){
+         for (int row = spaces; row < boardSize ; row++){
+             if(col <= spaces ) {
+                    if(board[col][row] == playerPiece && board[col + 1][row - 1] == playerPiece && board[col + 2][row - 2] == playerPiece && board[col + 3][row - 3] == playerPiece && board[col + 4][row - 4] == playerPiece) {
+                                    return true;
+                                }
+             }
+         }    
+     }
+     return false;
 }
-
-// The function below this line doesnt work 
-void checkTie(int _board[][100], void (*_startGame)(), int boardSize){
+bool checkTie(int **board, int boardSize) {
     Game game;
-    int count = 0;
+            int count = 0;
     for(int i = 0; i < boardSize; i++){
-        for(int j = 0; j < boardSize; j++){
-            if(_board[i][j] != 0) {
-                count++;
-            }
-            else if(_board[i][j] == 0){
-                count = count;
-            }
-        if(count >= (boardSize * boardSize)) {
-                    count = 0;
 
-                    _startGame();
+        for(int j = 0; j < boardSize; j++){
+     
+            if(board[i][j] != 0){
+                count ++;
             }
-            
+             if(count >= (boardSize*boardSize)){
+                return true;
+            }
+
         }
     }
+    return false;
+
 }
-// The function above this line doesnt work 
 
-// This start the game and executes every thing;
-void startGame()
+void startGame() 
 {
-    Game game; //This is an object of the game class;
-
-    ofstream Results("gomokuResults.txt"); // Here I create the output file named gomokuResults.txt;
-
-    srand(time(0)); /* This sets the seed for the random numbers generated -- 
-    This is so we don't get any patterns in the numbers generated */
-
-// Here Im keeping track of the scores for both players;
+    Game game;
+     srand (time(NULL));
+    ofstream GomokuResults("gomokuResults.txt");
     int PlayerOneWins = 0;
     int PlayerTwoWins = 0;
-    bool  Winner = false;
     int indexForGameTurn = 0; // This is an index for each turn of the game it is number of how many games have been played
     while(true)
     {
-    /* Here we are checking if the size we get from the file is now less or equal 5  because numbers less than 6 will return a memory 
-    error in our validation functions*/
        
-      if(readFromFile()[indexForGameTurn] > 5) 
-      {
-          game.BOARD_SIZE = readFromFile()[indexForGameTurn]; // Dynamically set the size of the board to the size in the Input.txt file;
-          Results<<"size_"<<game.BOARD_SIZE<<"\n"; //Print the size before every game
-        indexForGameTurn++; // Increase the number of the games played in each turn
-      }
-      //  This is if the size goes under the minimum board size which is 6; then we exit the loop;
-      else 
-      {
-          Results<<"wins alg1_"<<PlayerOneWins<<"\n"; // print the wins summary in the Results file in a new line.
-          Results<<"wins alg2_"<<PlayerTwoWins<<"\n"; // print the wins summary in the Results file in a new line.
-          break;
-      }
-  
-    int board[100][100] = {0}; // Initialize the board for each game; Setting it back to empty 
-//   Now this is the playing process.
-      while(true)
-      {
-          if(checkWin(board, game.alg_1, game.BOARD_SIZE)) 
-          {
-                Winner = true;
-                PlayerOneWins++; // Increase the number of wins for player one
-                cout<<"win_alg1"<<endl; //DELETE THIS LINE AFTER TESTING ********;
-                Results<<"win_alg1"<<"\n\n"; //Print the result if alg2 wins
-
-                break; //Game over we exit the loop and start a new game;
-          }
-          else if(checkWin(board, game.alg_2, game.BOARD_SIZE)) 
-          {  
-                Winner = true; 
-                PlayerTwoWins++;  // Increase the number of wins for player two
-                cout<<"win_alg2"<<endl; //DELET THIS LINE
-                Results<<"win_alg2"<<"\n\n"; //Print the result if alg2 wins
-                
-                break;  //Game over we exit the loop and start a new game;
-          }
-          else 
-          {
-             checkTie(board, startGame, game.BOARD_SIZE); // Call the check tie function
-          }
-
-          if (game.TURN == 0) //First Turn;
-          {
-              int x, y;
-            /* Change the values of X and Y to [cin>>x] and [cin>>y] FOR DEBUGING AND TESTING*/
-                x = rand() % game.BOARD_SIZE; //Genarate a random row
-    		    y = rand() % game.BOARD_SIZE; //Genarate a random column
-
-              while(game.getVal(board, x, y) != game.EMPTY_SPOT) //This is to make sure the same spot is not played twice I think it saves time I might be    wrong
-              {
-
-                  x = rand() % game.BOARD_SIZE; //Genarate a random row
-    		        y = rand() % game.BOARD_SIZE; //Genarate a random column
-
-              }
-
-              if (game.getVal(board, x, y)== game.EMPTY_SPOT) 
-              {
-                cout<<"r"<<y << "c"<<x<<"      alg"<< game.alg_1<< endl; //DELETE THIS TOO
-
-                Results<<"r"<<y<<"c"<<x<<" alg"<< game.alg_1<<"\n"; // Print the move for player one in the results file
-    		    game.setMove(board,x, y, game.alg_1); // Set the move in the board
-
-                  game.TURN += 1; //Increase Turn Buy One; Here Turn Is Going To Be One
-    		    }
-              else 
-              {
-                  game.TURN = game.TURN; // This is here incase for any reason the algo plays an invalid spot we make it play again
-              }
-    		}
-
-      else
-      {
-          int x, y;
-
-    		x = rand() % game.BOARD_SIZE;
-    		y = rand() % game.BOARD_SIZE;
-
-          while(game.getVal(board, x, y) != game.EMPTY_SPOT) //This is to make sure the same spot is not played twice I think it saves time I might be wrong
-          {
-            x = rand() % game.BOARD_SIZE;
-    		y = rand() % game.BOARD_SIZE;
-          }
-         if (game.getVal(board, x, y) == game.EMPTY_SPOT) 
-         {
-            cout<<"r"<<y << "c"<< x <<"        alg"<< game.alg_2<< endl; //DELETE LINE
-
-            Results<<"r"<<y<<"c"<<x<<" alg"<< game.alg_2<<"\n";  // Print the move for player one in the results file
-
-    		game.setMove(board,x, y, game.alg_2); // Set the move in the board
-
-            game.TURN -= 1; // Decrement the turn 
-    	}
-        else 
-        {
-            game.TURN = game.TURN; // Dont change turn if move is invalid
+        if(readFromFile()[indexForGameTurn] <= 0 || readFromFile()[indexForGameTurn] > 20){
+            GomokuResults<<"wins alg1_"<<PlayerOneWins<<endl;
+            GomokuResults<<"wins alg2_"<<PlayerTwoWins<<endl;
+            break;
         }
-      } 
+        game.BOARD_SIZE = readFromFile()[indexForGameTurn];
+        GomokuResults<<"size_"<<game.BOARD_SIZE<<"\n"; //Print the size before every game       
+        game.createBoard(game.BOARD_SIZE);
+        indexForGameTurn++; 
 
-         game.printBoard(board, game.BOARD_SIZE); //DELETE LINE
-      }
+        
+     
+    while(true) {
+    if(checkWin(game.board, game.alg_1, game.BOARD_SIZE)) {
+            GomokuResults<<"win_alg1"<<"\n"<<endl;
+            PlayerOneWins++; // Increase the number of wins for player one
+            break;
+        }
+     if(checkWin(game.board, game.alg_2, game.BOARD_SIZE)) {
+            GomokuResults<<"win_alg2"<<"\n"<<endl;
+            PlayerTwoWins++; // Increase the number of wins for player one
+            break;
+        }
+        if(checkTie(game.board, game.BOARD_SIZE)){
+        break;
+        };
 
+        if(game.TURN == 0) {
+                int x, y;
+                  x = rand() % game.BOARD_SIZE;
+                    y = rand() % game.BOARD_SIZE;
+                while(game.getVal(game.board, y, x) != game.EMPTY_SPOT){
+                    x = rand() % game.BOARD_SIZE;
+                    y = rand() % game.BOARD_SIZE;
+                }
+            if(game.getVal(game.board, y, x) == game.EMPTY_SPOT){
+                GomokuResults<<"r"<<y<<"c"<<x<<" alg1"<<endl;
+                game.setMove(game.board, y, x, game.alg_1);
+                game.TURN += 1;
+               
+            }
+        }
+        else  {
+                int x, y;
+                x = rand() % game.BOARD_SIZE;
+                y = rand() % game.BOARD_SIZE;
+                while(game.getVal(game.board, y, x) != game.EMPTY_SPOT){
+                    x = rand() % game.BOARD_SIZE;
+                    y = rand() % game.BOARD_SIZE;
+                }
+                if(game.getVal(game.board, y, x) == game.EMPTY_SPOT){
+                    GomokuResults<<"r"<<y<<"c"<<x<<" alg2"<<endl;
+                game.setMove(game.board, y, x, game.alg_2);
+                game.TURN -=  1;
+            }
+        }      
+        game.printBoard(game.board, game.BOARD_SIZE);
     }
-    Results.close(); // Close the file after the games are done;
-    _Exit(10);
+
 }
- 
 
-
+}
 
 
 int main() {
     startGame();
     cin.get();
+    return  0;
 }
